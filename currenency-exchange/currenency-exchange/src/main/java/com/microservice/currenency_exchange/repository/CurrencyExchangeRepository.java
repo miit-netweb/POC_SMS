e46db71305c7794dd26c284ab0c2f3f4d590d2ae
miit-netweb <cchess338@gmail.com>
2024-07-15 11:02:12 +0530

    Updated SMS_POC

--- currenency-exchange/currenency-exchange/src/main/java/com/microservice/currenency_exchange/repository/CurrencyExchangeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservice.currenency_exchange.repository;
-
-import com.microservice.currenency_exchange.bean.CurrencyExchange;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange,Long> {
-
-    public CurrencyExchange findByFromAndTo(String from,String to);
-}
